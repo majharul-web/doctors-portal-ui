03ce6d4106036ac6dddbf2273398026c73fc2ae3
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-11-02 20:19:39 +0600

    add navigation  and services

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+
+const Service = (props) => {
+    const { name, description, img } = props.service;
+    return (
+        <Grid item xs={2} sm={4} md={4}>
+            <Card sx={{ minWidth: 275, border: 0, boxShadow: 0 }}>
+                <CardMedia
+                    component="img"
+                    style={{ width: 'auto', margin: '0 auto' }}
+                    image={img}
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        {name}
+                    </Typography>
+
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {description}
+                    </Typography>
+
+                </CardContent>
+
+            </Card>
+
+        </Grid>
+    );
+};
+
+export default Service;
